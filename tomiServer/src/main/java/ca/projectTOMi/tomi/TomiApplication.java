f235da6c3994cf0a01c7fc957dc78f673b60f5e5
Karol Talbot <757826@ACDM.DS.SAIT.CA>
2019-01-07 19:14:54 -0700

    Initial Project creation
    Project is freshly setup using spring-initializor for the server and angular for the client. Some intial step up was completed to allow for connection to a local postgresql database

---
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/TomiApplication.java
@@ -0,0 +1,14 @@
+package ca.projectTOMi.tomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomiApplication.class, args);
+	}
+
+}
+
