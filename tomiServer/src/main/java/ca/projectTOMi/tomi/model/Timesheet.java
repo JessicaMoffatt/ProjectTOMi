353d0a875c5ca5c21dd82539904f20e04903f02d
James <jamieandrade@hotmail.com>
2019-01-11 12:49:23 -0700

    Added TimesheetController, Status (enum), and Timesheet

--- tomiServer/src/main/java/ca/projectTOMi/tomi/model/Timesheet.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/model/Timesheet.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 
 /**
  * class representing a single timesheet for a user for a specific week.  Entries (task ,client, hours)
- * on the timesheet are attacched by referencing it.
+ * on the timesheet are attached by referencing it.
  */
 
 @Entity
