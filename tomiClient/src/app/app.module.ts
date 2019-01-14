beab17d23e994afcc347e44d0a89205d64834f0c
James <jamieandrade@hotmail.com>
2019-01-14 11:37:29 -0700

    Basic top nav bar created for front end

--- tomiClient/src/app/app.module.ts
+++ tomiClient/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { ApprovePanelComponent } from './approve-panel/approve-panel.component';
+import { TimesheetPanelComponent } from './timesheet-panel/timesheet-panel.component';
+import { TeamPanelComponent } from './team-panel/team-panel.component';
+import { ProjectsPanelComponent } from './projects-panel/projects-panel.component';
+import { ManageTeamsPanelComponent } from './manage-teams-panel/manage-teams-panel.component';
+import { UnitTypesPanelComponent } from './unit-types-panel/unit-types-panel.component';
+import { TasksPanelComponent } from './tasks-panel/tasks-panel.component';
+import { UserAccountsPanelComponent } from './user-accounts-panel/user-accounts-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    ApprovePanelComponent,
+    TimesheetPanelComponent,
+    TeamPanelComponent,
+    ProjectsPanelComponent,
+    ManageTeamsPanelComponent,
+    UnitTypesPanelComponent,
+    TasksPanelComponent,
+    UserAccountsPanelComponent
   ],
   imports: [
     BrowserModule,
