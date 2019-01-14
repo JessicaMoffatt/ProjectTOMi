07a2539fc6844cedf2913458fcf12cf3fb75ffd7
Iliya <iliya.kiritchkov@hotmail.com>
2019-01-14 11:50:59 -0700

    Tryna make a change

--- tomiServer/src/main/java/ca/projectTOMi/tomi/service/TaskService.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/service/TaskService.java
@@ -1,5 +1,6 @@
 package ca.projectTOMi.tomi.service;
 
+import ca.projectTOMi.tomi.exception.TaskNotFoundException;
 import ca.projectTOMi.tomi.model.Task;
 import ca.projectTOMi.tomi.persistence.TaskRepository;
 
@@ -16,14 +17,33 @@ public class TaskService {
      * Constructor for the TaskService component.
      * @param repository Repository responsible for persisting {@link Task} instances.
      */
-    public TaskRepository(TaskRepository repository) { this.repository = repository; }
+    public TaskService(TaskRepository repository) { this.repository = repository; }
 
+    /**
+     * Updates the {@link Task} with the provided id with the provided attributes.
+     *
+     * @param id the unique identifier for the Task to update.
+     * @param newTask Task object containing the updated attributes.
+     * @return Task containing the updated attributes.
+     */
     public Task updateTask(Long id, Task newTask) {
         return repository.findById(id).map(task -> {
             task.setName(newTask.getName());
             task.setBillable(newTask.isBillable());
             task.setActive(newTask.isActive());
             return repository.save(task);
-        }).orElse(() -> new TaskNotFoundException());
+        }).orElseThrow(() -> new TaskNotFoundException());
     }
+
+    /**
+     * Gets a {@link Task} object with the provided id.
+     *
+     * @param id the unique identifier for the Task to find.
+     * @return Task object matching the provided id.
+     */
+    public Task getTask(Long id) {
+        return repository.findById(id).orElseThrow(() -> new TaskNotFoundException());
+    }
+
+
 }
