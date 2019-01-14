e8f9699511fb3248df26ff19ddf961f7ec48bb2a
Iliya <iliya.kiritchkov@hotmail.com>
2019-01-14 16:22:04 -0700

    Completed the TaskController and TaskResourceAssembler

--- tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/TaskResourceAssembler.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/TaskResourceAssembler.java
@@ -1,12 +1,19 @@
 package ca.projectTOMi.tomi.assembler;
 
+import ca.projectTOMi.tomi.controller.TaskController;
 import ca.projectTOMi.tomi.model.Task;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 
+import java.net.URISyntaxException;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 /**
- * TaskResourceAssembler is responsible for creating a standard resource for {@link Task}
+ * TaskResourceAssembler is responsible for creating a standard resource for {@link Task}.
+ *
  * @author Iliya Kiritchkov
  * @version 1
  */
@@ -16,7 +23,19 @@ public class TaskResourceAssembler implements ResourceAssembler<Task, Resource<T
     @Override
     public Resource<Task> toResource(Task task) {
         Resource<Task> resource = new Resource<>(task,
-                linkTo(methodOn(TaskController.class)));
+                linkTo(methodOn(TaskController.class).getTask(task.getId())).withSelfRel(),
+                linkTo(methodOn(TaskController.class).getActiveTasks()).withRel("tasks"),
+                linkTo(methodOn(TaskController.class).getActiveAndBillableTasks()).withRel("tasks/billable"),
+                linkTo(methodOn(TaskController.class).getActiveAndNonBillableTasks()).withRel("tasks/nonbillable"),
+                linkTo(methodOn(TaskController.class).setTaskInactive(task.getId())).withRel("delete")
+        );
 
+        try {
+            resource.add(linkTo(methodOn(TaskController.class).updateTask(task.getId(), task)).withRel("update"));
+        } catch (URISyntaxException e) {
+            System.out.println(e);
+        }
+
+        return resource;
     }
 }
--- tomiServer/src/main/java/ca/projectTOMi/tomi/service/TaskService.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/service/TaskService.java
@@ -3,6 +3,7 @@ package ca.projectTOMi.tomi.service;
 import ca.projectTOMi.tomi.exception.TaskNotFoundException;
 import ca.projectTOMi.tomi.model.Task;
 import ca.projectTOMi.tomi.persistence.TaskRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
  * @author Iliya Kiritchkov
  * @version 1
  */
+@Component
 public class TaskService {
     private TaskRepository repository;
 
