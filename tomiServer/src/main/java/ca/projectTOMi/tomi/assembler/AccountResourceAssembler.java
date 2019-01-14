8e668b05f41eadbabd9a429f9319bdd28131bd24
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-01-13 18:06:14 -0700

    Completed AccountController and Javadoc for AccountServices and AccountController
    Some minor changes were made to TeamServices and TeamController. Most changes were only to javadoc when typos were discovered.

--- tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/AccountResourceAssembler.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/AccountResourceAssembler.java
@@ -1,7 +1,9 @@
 package ca.projectTOMi.tomi.assembler;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.net.URISyntaxException;
 import ca.projectTOMi.tomi.controller.AccountController;
 import ca.projectTOMi.tomi.model.Account;
 import org.springframework.hateoas.Resource;
@@ -9,6 +11,8 @@ import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 
 /**
+ * AccountResourceAssembler is responsible for creating a standard resource for {@link Account}
+ * objects.
  *
  * @author Karol Talbot
  * @version 1
@@ -20,8 +24,16 @@ public class AccountResourceAssembler implements ResourceAssembler<Account, Reso
   public Resource<Account> toResource(Account account) {
     Resource<Account> resource = new Resource<>(account,
       linkTo(methodOn(AccountController.class).getAccount(account.getId())).withSelfRel(),
-      linkTo(methodOn(AccountController.class).getActiveAccounts()).withRel("accounts")
+      linkTo(methodOn(AccountController.class).getActiveAccounts()).withRel("accounts"),
+      linkTo(methodOn(AccountController.class).setAccountInactive(account.getId())).withRel("delete")
     );
+
+    try {
+      resource.add(linkTo(methodOn(AccountController.class).updateAccount(account.getId(), account)).withRel("update"));
+    } catch (URISyntaxException e) {
+      System.out.println(e);
+    }
+
     return resource;
   }
 }
--- tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/TeamResourceAssembler.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/assembler/TeamResourceAssembler.java
@@ -1,9 +1,10 @@
 package ca.projectTOMi.tomi.assembler;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.net.URISyntaxException;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-
+import ca.projectTOMi.tomi.controller.AccountController;
 import ca.projectTOMi.tomi.controller.TeamController;
 import ca.projectTOMi.tomi.model.Team;
 import org.springframework.hateoas.Resource;
@@ -17,19 +18,20 @@ import org.springframework.stereotype.Component;
  * @version 1
  */
 @Component
-public class TeamResourceAssembler implements ResourceAssembler<Team, Resource<Team>>{
+public class TeamResourceAssembler implements ResourceAssembler<Team, Resource<Team>> {
 
   @Override
   public Resource<Team> toResource(Team team) {
     Resource<Team> resource = new Resource<>(team,
       linkTo(methodOn(TeamController.class).getTeam(team.getId())).withSelfRel(),
       linkTo(methodOn(TeamController.class).getActiveTeams()).withRel("teams"),
-      linkTo(methodOn(TeamController.class).setTeamInactive(team.getId())).withRel("delete")
+      linkTo(methodOn(TeamController.class).setTeamInactive(team.getId())).withRel("delete"),
+      linkTo(methodOn(AccountController.class).getAccountsByTeam(team.getId())).withRel("getAccounts")
     );
 
     try {
       resource.add(linkTo(methodOn(TeamController.class).updateTeam(team.getId(), team)).withRel("update"));
-    }catch (URISyntaxException e){
+    } catch (URISyntaxException e) {
       System.out.println(e);
     }
 
