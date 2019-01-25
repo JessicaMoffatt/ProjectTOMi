54e81ac3c2779eb6dc4df053016c1d392da2b758
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-01-25 08:10:51 -0700

    Update TeamController.java

--- tomiServer/src/main/java/ca/projectTOMi/tomi/controller/TeamController.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/controller/TeamController.java
@@ -3,6 +3,8 @@ package ca.projectTOMi.tomi.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 import ca.projectTOMi.tomi.assembler.TeamResourceAssembler;
