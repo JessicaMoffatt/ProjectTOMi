12aa303f0c19773aeec02833c748460db1223b1c
Iliya Kiritchkov <382002@ACDM.DS.SAIT.CA>
2019-01-25 12:25:06 -0700

    changed uri to /team and added link to resourceAssembler

--- tomiServer/src/main/java/ca/projectTOMi/tomi/controller/UserAccountController.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/controller/UserAccountController.java
@@ -157,7 +157,7 @@ public class UserAccountController {
    * @param teamId the unique identifier for the Team
    * @return List of UserAccounts that are active, not part of the provided Team and not a team lead of any Teams.
    */
-  @GetMapping ("/user_accounts/{teamId}/available")
+  @GetMapping ("/teams/{teamId}/available")
   public Resources<Resource<UserAccount>> getAvailableUserAccountsForTeam(@PathVariable Long teamId) {
 
     List<Resource<UserAccount>> userAccount = service.getAvailableUserAccountsForTeam(teamId).stream().map(assembler::toResource).collect(Collectors.toList());
