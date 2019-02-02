cab210ded904bc35d5a06cc8c9d64146e4f41008
JessicaMoffatt <JessicaMoffatt@users.noreply.github.com>
2019-02-02 16:04:38 -0700

    commit before update form master

--- tomiClient/src/app/model/entry.ts
+++ tomiClient/src/app/model/entry.ts
@@ -6,31 +6,36 @@
  * @author Jessica Moffatt
  * @version 1.0
  */
+import {UserAccount} from "./userAccount";
+import {Project} from "./project";
+import {Task} from './task';
+import {UnitType} from "./unitType";
+import {Status} from "./status";
+
 export class Entry{
   //TODO explain what each number means, fix date and hours,
   // project isn't a string, task isn't a string, unitType isn't a string
   /** Represents the approval status of this entry.*/
   approved: number;
-  /** The name of the project this entry is for.*/
-  projectName: string;
-  /** The list of days of the week.*/
-  date: Date[];
-  /** The corresponding hours worked for each day of the week.*/
-  hours: number[];
   /** The project this entry is for.*/
-  project: string;
+  project: Project;
   /** The task this entry is for.*/
-  task: string;
+  task: Task;
   /** The component worked on for this entry.*/
   component: string;
   /** The number of units produced for this entry.*/
   quantity: number;
   /** The unit type corresponding to the component worked on for this entry.*/
-  unitType: string;
+  unitType: UnitType;
   /** The unique identifier for this entry.*/
   id: number;
+  /** The user account for which this entry is for.*/
+  owner: UserAccount;
+  /** Represents whether or not the entry has been approved by the project manager.*/
+  status: Status;
+  /** The Monday of the week this Entry is for.*/
+  date: Date;
 
-  //TODO assign approved to the correct number
   constructor(){
     this.approved = 1;
     this.projectName = "";
@@ -42,5 +47,6 @@ export class Entry{
     this.quantity = 0;
     this.unitType = "";
     this.id = -1;
+    this.owner = null;
   }
 }
