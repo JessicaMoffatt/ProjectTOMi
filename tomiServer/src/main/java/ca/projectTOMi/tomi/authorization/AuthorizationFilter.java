c0b557325b3348aa77a3d41e701b696f67e54348
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-02-19 21:27:10 -0700

    Update AuthorizationFilter.java

--- tomiServer/src/main/java/ca/projectTOMi/tomi/authorization/AuthorizationFilter.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/authorization/AuthorizationFilter.java
@@ -2,23 +2,23 @@ package ca.projectTOMi.tomi.authorization;
 
 import java.util.List;
 public interface AuthorizationFilter<E> {
-  /**
-   * Removes items from a list that a UserAccount does not have permission to view.
-   *
-   * @param list
-   *   list of E to be filtered
-   *
-   * @return List of filtered Items
-   */
-  List<E> filterList(List<E> list);
+	/**
+	 * Removes items from a list that a UserAccount does not have permission to view.
+	 *
+	 * @param list
+	 * 	list of E to be filtered
+	 *
+	 * @return List of filtered Items
+	 */
+	List<E> filterList(List<E> list);
 
-  /**
-   * Filters fields from an object E that a UserAccount does not have permission to view.
-   *
-   * @param toFilter
-   *   E to be filtered
-   *
-   * @return filtered item E
-   */
-  E filterFields(E toFilter);
+	/**
+	 * Filters fields from an object E that a UserAccount does not have permission to view.
+	 *
+	 * @param toFilter
+	 * 	E to be filtered
+	 *
+	 * @return filtered item E
+	 */
+	E filterFields(E toFilter);
 }
