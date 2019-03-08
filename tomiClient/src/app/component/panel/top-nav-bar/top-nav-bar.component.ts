a89314d57cc6584f27695791cfc62253539ae51c
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-03-08 11:10:57 -0700

    checking iliya's stuff

--- tomiClient/src/app/component/panel/top-nav-bar/top-nav-bar.component.ts
+++ tomiClient/src/app/component/panel/top-nav-bar/top-nav-bar.component.ts
@@ -1,5 +1,6 @@
 import {Component, NgModule, OnInit} from '@angular/core';
 import {SignInService} from "../../../service/sign-in.service";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 
 @NgModule({
@@ -11,16 +12,52 @@ import {SignInService} from "../../../service/sign-in.service";
   styleUrls: ['./top-nav-bar.component.scss']
 })
 export class TopNavBarComponent implements OnInit {
-  signInService:SignInService;
+  signInService: SignInService;
   isUserLoggedIn: boolean;
+  screenSize: number;
 
-  constructor(signInService:SignInService) {
+  constructor(signInService: SignInService, breakpointObserver: BreakpointObserver) {
     this.signInService = signInService;
-    this.signInService.isUserLoggedIn.subscribe( value => {
+    this.signInService.isUserLoggedIn.subscribe(value => {
       this.isUserLoggedIn = value;
+    });
+    breakpointObserver.observe([
+      Breakpoints.XSmall
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateSmallLayout();
+      }
+    });
+    breakpointObserver.observe([
+      Breakpoints.Small,
+      Breakpoints.Medium
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateMediumLayout();
+      }
+    });
+    breakpointObserver.observe([
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateLargeLayout();
+      }
     });
   }
 
   ngOnInit() {
+  }
+
+  private activateSmallLayout() {
+    this.screenSize = 2;
+  }
+
+  private activateMediumLayout() {
+    this.screenSize = 1;
+  }
+
+  private activateLargeLayout() {
+    this.screenSize = 0;
   }
 }
