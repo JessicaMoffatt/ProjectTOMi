b2dc3ad6a17b22d54a205d2e36709be761ff9ea5
Jessica Moffatt <756732@ACDM.DS.SAIT.CA>
2019-03-28 12:51:25 -0600

    okay, now data dump works

--- tomiClient/src/app/component/panel/projects-panel/projects-panel.component.ts
+++ tomiClient/src/app/component/panel/projects-panel/projects-panel.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Project} from "../../../model/project";
 import {ProjectService} from "../../../service/project.service";
 import {DatePipe} from '@angular/common';
-import {MatSnackBar} from "@angular/material";
+import {MatSnackBar, MatTab, MatTabChangeEvent} from "@angular/material";
 import {BehaviorSubject} from "rxjs";
 import {UserAccount} from "../../../model/userAccount";
 import {SignInService} from "../../../service/sign-in.service";
+import {DataDumpReportComponent} from "../../extra/data-dump-report/data-dump-report.component";
 
 @Component({
   selector: 'app-projects-panel',
   templateUrl: './projects-panel.component.html',
   styleUrls: ['./projects-panel.component.scss']
 })
-
-
 export class ProjectsPanelComponent implements OnInit {
+
+  @ViewChild('dumpTab') dumpTab: MatTab;
+  @ViewChild('dataDumpReport') dataDumpReport: DataDumpReportComponent;
 
   constructor(private projectService: ProjectService,
               public snackBar:MatSnackBar, public signInService:SignInService) {
@@ -27,7 +29,10 @@ export class ProjectsPanelComponent implements OnInit {
     this.projectService.userAccountList = new BehaviorSubject<Array<UserAccount>>([]);
   }
 
-
-
+  tabEvent(event:MatTabChangeEvent){
+    if(event.tab === this.dumpTab){
+      this.dataDumpReport.getDataDump();
+    }
+  }
 
 }
