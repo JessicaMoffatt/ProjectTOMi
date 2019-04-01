42aaef31b911565b7b9e62edcb178f6d7008a22c
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-04-01 11:06:20 -0600

    Fixed slide toggle

--- tomiServer/src/main/java/ca/projectTOMi/tomi/service/ProjectService.java
+++ tomiServer/src/main/java/ca/projectTOMi/tomi/service/ProjectService.java
@@ -1,7 +1,6 @@
 package ca.projectTOMi.tomi.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import ca.projectTOMi.tomi.exception.ProjectManagerException;
@@ -66,7 +65,6 @@ public final class ProjectService {
 			this.projectAuthService.changeProjectManager(project, newProject);
 
 			project.setProjectManager(newProject.getProjectManager());
-			project.setProjectMembers(newProject.getProjectMembers());
 			project.setActive(true);
 			return this.repository.save(project);
 		}).orElseThrow(ProjectNotFoundException::new);
