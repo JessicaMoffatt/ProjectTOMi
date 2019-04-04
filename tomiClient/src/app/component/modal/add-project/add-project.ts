2f80fe48adfa5f157b103888e94036ddb20f8658
KarolTalbot <44789667+KarolTalbot@users.noreply.github.com>
2019-04-03 20:15:24 -0600

    Add projects mostly working just need new client adding code

--- tomiClient/src/app/component/modal/add-project/add-project.ts
+++ tomiClient/src/app/component/modal/add-project/add-project.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material";
+import {ClientService} from "../../../service/client.service";
+import {Project} from "../../../model/project";
 
 /**
  * AddTaskComponent is a modal form used to add a new Task to the back end.
@@ -15,8 +17,23 @@ import {MatDialogRef} from "@angular/material";
 })
 export class AddProjectComponent implements OnInit {
 
+  accountManagerControl = new FormControl('', [
+    Validators.required
+  ]);
 
   projectNameControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  clientControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  billingControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  budgetControl = new FormControl('', [
     Validators.required
   ]);
 
@@ -26,7 +43,8 @@ export class AddProjectComponent implements OnInit {
   /** The ngForm for this component */
   @ViewChild('addProjectForm') addProjectForm;
 
-  constructor(public dialogRef: MatDialogRef<AddProjectComponent>) { }
+  constructor(public dialogRef: MatDialogRef<AddProjectComponent>, public clientService: ClientService) {
+  }
 
   /**
    * Closes the modal component.
@@ -40,7 +58,22 @@ export class AddProjectComponent implements OnInit {
   }
 
   private async addProject() {
-    if (this.projectNameControl.valid) {
+    let initials: string;
+    let project: Project;
+    if (this.projectNameControl.valid && this.accountManagerControl && this.billingControl && this.budgetControl && this.clientControl) {
+      project = new Project();
+      initials = '';
+      project.projectName = this.projectNameControl.value;
+      project.billableRate = this.billingControl.value;
+      project.budget = this.budgetControl.value;
+      this.accountManagerControl.value.toString().split(" ").forEach(name => {
+        if (initials.length < 2) {
+          initials += name.charAt(0).toUpperCase();
+        }
+      });
+      project.id = initials;
+      project.client = this.clientControl.value;
+      console.log(project);
       this.dialogRef.close();
     }
   }
